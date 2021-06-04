019f59bd259e8b6de2a92ed55c8beb84296e0756
Titux Metal <github@lgdweb.fr>
2021-06-04 10:14:33 +0200

    Updating main.js file in routes; GET / adds more details to the response of the main endpoint

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,6 +2,13 @@ import { Router } from 'express'
 
 const router = new Router()
 
-router.get('/', async (_req, res) => res.json({ message: 'Hello Trip Cost API'}))
+router.get('/', async (_req, res) =>
+  res.json({
+    name: 'fullstack-bootcamp-trip-cost-node-api',
+    description:
+      'A simple api that calculate the cost of your trip based on expenses you make, eg.gasoline, hotels, food, tickets..',
+    url: 'https://github.com/TituxMetal/fullstack-bootcamp-trip-cost-node-api'
+  })
+)
 
 export default router
