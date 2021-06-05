4567c974126d187e18b7cfecf488956ae416c833
Titux Metal <github@lgdweb.fr>
2021-06-05 19:45:30 +0200

    Updating trips.js file in routes; implements POST /api/trips route Create trip

--- src/routes/trips.js
+++ src/routes/trips.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 
+import { Trips } from '../collections'
+
 const router = new Router()
 
 router.get('/', async (_req, res) => {
@@ -15,13 +17,32 @@ router.get('/', async (_req, res) => {
 
 router.post('/', async (req, res) => {
   const { name } = req.body
+
+  if (typeof name !== 'string') {
+    return res.status(422).json({
+      type: 'Validation Error',
+      reason: 'Name must be a string value',
+      status: 422
+    })
+  }
+
+  if (!name || name.length < 4) {
+    return res.status(422).json({
+      type: 'Validation Error',
+      reason: 'Name must be at least 4 characters long',
+      status: 422
+    })
+  }
+
+  const { insertedId } = await Trips.createTrip({ name })
+
   const response = {
-    data: { id: '', name },
+    data: { id: insertedId },
     message: 'Trip created',
     status: '201 CREATED'
   }
 
-  res.status(201).json(response)
+  return res.status(201).json(response)
 })
 
 export default router
